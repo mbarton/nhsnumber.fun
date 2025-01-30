b22335ae6e1a0d5157178cc8b5a11e3d72a1a479
Michael Barton <mbarton@users.noreply.github.com>
2025-01-30 11:09:28 +0000

    Now you can generate loads of fake numbers what a time to be alive

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 import nhs_number
 
 def handler(event, context):
+    path = event["requestContext"]["http"]["path"]
+    quantity = 1
+
+    if path != "/":
+        quantity = max(int(path[1:]), 1000)
+
+    numbers = nhs_number.generate(quantity=quantity)
+
     return {
         "statusCode": 200,
         "headers": {
             "Content-Type": "text/plain"
         },
-        "body": nhs_number.generate()[0]
+        "body": "\n".join(numbers)
     }
